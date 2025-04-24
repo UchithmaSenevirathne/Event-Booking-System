595e52f7fc929c522c21a474181de15404e1cee4
UchithmaSenevirathne <uchithma@gmail.com>
2025-04-24 19:58:15 +0530

    create User Repository

---
+++ backend/src/main/java/lk/ijse/backend/controllers/UserController.java
@@ -0,0 +1,11 @@
+package lk.ijse.backend.controllers;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("events/backend/user")
+@CrossOrigin
+public class UserController {
+}
