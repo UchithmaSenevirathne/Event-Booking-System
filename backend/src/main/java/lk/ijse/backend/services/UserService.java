e2b7b40565bd1847e53d575422e95d9bbf1d7509
UchithmaSenevirathne <uchithma@gmail.com>
2025-04-24 20:00:34 +0530

    create User service and service impl

---
+++ backend/src/main/java/lk/ijse/backend/services/UserService.java
@@ -0,0 +1,4 @@
+package lk.ijse.backend.services;
+
+public interface UserService {
+}
---
+++ backend/src/main/java/lk/ijse/backend/services/impl/UserServiceIMPL.java
@@ -0,0 +1,17 @@
+package lk.ijse.backend.services.impl;
+
+import lk.ijse.backend.services.UserService;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+public class UserServiceIMPL implements UserService, UserDetailsService {
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return null;
+    }
+}
